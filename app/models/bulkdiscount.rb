73522d0b2e377237970b1965a495f97345b34d15
Ben Silverstein <benjsilverstein@comcast.net>
2022-08-09 21:40:14 -0600

    Add percentage validation for edge cases

--- app/models/bulkdiscount.rb
+++ app/models/bulkdiscount.rb
@@ -2,6 +2,8 @@ class Bulkdiscount < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :percentage
   validates_presence_of :threshold, numericality: true
+  validates_numericality_of :percentage, less_than_or_equal_to: 100
+  validates_numericality_of :percentage, greater_than_or_equal_to: 1
 
   belongs_to :merchant
 
